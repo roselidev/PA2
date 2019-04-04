e48be21e94575af7ec8f47cf35d1c46eb853cbbf
Jin-Soo Kim <jinsoox@gmail.com>
2019-04-04 09:00:20 +0900

    initial commit

---
+++ pa2.c
@@ -0,0 +1,53 @@
+//--------------------------------------------------------------
+// 
+//  4190.308 Computer Architecture (Spring 2019)
+//
+//  Project #2: TinyFP Representation
+//
+//  April 4, 2019
+//
+//  Jin-Soo Kim (jinsoo.kim@snu.ac.kr)
+//  Systems Software & Architecture Laboratory
+//  Dept. of Computer Science and Engineering
+//  Seoul National University
+//
+//--------------------------------------------------------------
+
+#include <stdio.h>
+#include "pa2.h"
+
+
+tinyfp fixp2tinyfp(fixp f)
+{
+
+
+
+	return 9;
+}
+
+
+fixp tinyfp2fixp(tinyfp t)
+{
+
+
+
+	return 9;
+}
+
+
+tinyfp float2tinyfp(float f)
+{
+
+
+
+	return 9;
+}
+
+
+float tinyfp2float(tinyfp t)
+{
+
+
+
+	return 9.9;
+}
