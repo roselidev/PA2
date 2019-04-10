1a4f5940dc7a703222556f631b9c932291348790
lyo9608 <lyo9608@gmail.com>
2019-04-10 21:42:03 +0900

    Complete_1

--- pa2.c
+++ pa2.c
@@ -22,6 +22,7 @@ tinyfp fixp2tinyfp(fixp f)
   //declare answer
   tinyfp ans=0b00000000;
   int minus = 0;
+  int denorm=0;
 
   //check sign
   if((f>>31)&1){
@@ -34,9 +35,10 @@ tinyfp fixp2tinyfp(fixp f)
   //rough normalization
 
   int tmp = f>>10;
-  int E;
+  int E=0;
   //    infinite zone
   if(tmp>=16){
+    if(minus){ans+=1<<7;}
     ans = ans | 0b01110000;return ans;
   }
   //    normalize zone
@@ -44,26 +46,28 @@ tinyfp fixp2tinyfp(fixp f)
   else if(tmp>=4)E=2;
   else if(tmp>=2)E=1;
   else if(tmp==1)E=0;
-  else if(f>=(1<<9))E=-1;
-  else if(f>=(1<<8))E=-2;
+  else if(((int)f)>=(1<<9))E=-1;
+  else if(((int)f)>=(1<<8))E=-2;
+
 
   //    denormalize zone
-  else if(f>=(1<<3))E=-3;
-  else{ans= ans & 0b00000000;return ans;}//set as +0.0
+  else if(((int)f)>=(1<<3)){E=-2;denorm=1;}
+  else{ans= 0b00000000;if(minus){ans+=(1<<7);}return ans;}//set as 0
+
 
   //
   //Rounding
   //
  
   //  get GRS bit values
-  unsigned int Gbit = f & 1<<(6+E);
-  unsigned int Rbit = f & 1<<(5+E);
-  unsigned int Sbit = f & ((1<<(5+E))-1);
+  int Gbit = f & 1<<(6+E);
+  int Rbit = f & 1<<(5+E);
+  int Sbit = f & ((1<<(5+E))-1);
   
   //  Round up condition
-  if((Rbit==1 && Sbit!=0) || (Gbit==1 && Rbit==1 && Sbit==0)){
+  if((Rbit!=0 && Sbit!=0) || (Gbit!=0 && Rbit!=0 && Sbit==0)){
     f += 1<<(6+E);
-    if(f & (1<<(10+E))) E++;//If carry occurs, renormalize
+    if(f & (1<<(11+E))){E++;}//If carry occurs, renormalize
     if(E>3){ans = ans | 0b01110000; return ans;}//check infinity
   }
 
@@ -71,15 +75,16 @@ tinyfp fixp2tinyfp(fixp f)
   //Assign exp bits and frac bits to answer
   //
   
-  unsigned int exp = (E+BIAS)<<4;
+  int exp = (E+BIAS)<<4;
+  if(denorm==1){exp=0;}
   ans += exp;
-  unsigned int frac = (f & ((1<<(9+E)) + (1<<(8+E)) + (1<<(7+E)) + (1<<(6+E))));
+  int frac = (f & ((1<<(9+E)) + (1<<(8+E)) + (1<<(7+E)) + (1<<(6+E))));
   frac = frac>>(6+E);
   ans += frac;
 
   //check sign bit
   if(minus==1){
-    ans += (1<<31);
+    ans += (1<<7);
   }
 
   return ans;
@@ -95,11 +100,11 @@ fixp tinyfp2fixp(tinyfp t)
   //check sign bit
   if((t>>7)==1){
     minus = 1;
-    t-=(1>>7);
+    t-=(1<<7);
   }
 
   //when t is zero or infinity or NaN
-  if(t == 0b00000000){
+  if(t & 0b00000000){
     ans = 0x00000000;return ans;
   }
   else if(t>>4 == 0b0111){
@@ -108,11 +113,13 @@ fixp tinyfp2fixp(tinyfp t)
   //when t is normalized / denormalized non-zero value
   else{
     if((t>>4)==0b0000){
-      ans = ans + (t<<3);
+      ans = ans + (t<<4);
     }
     else{
       int E = (t>>4) - BIAS;
-      ans = ans + (t<<(5+E));
+      t = t & 0b0001111;//remove all exp part and make normalize form
+      t += 1<<4;
+      ans = ans + (t<<(6+E));
     }
 
     //check sign bit
@@ -126,20 +133,155 @@ fixp tinyfp2fixp(tinyfp t)
 	return 9;
 }
 
+union bitfloat{
+  float flo;
+  int bit;
+};
 
 tinyfp float2tinyfp(float f)
 {
+  //declare answer
+  tinyfp ans=0b00000000;
+  int minus=0;
+  union bitfloat F;
+  F.flo=f;
+  int frac = F.bit & 0x007fffff;
+
+  //check sign value
+  if(F.bit>>31){
+    minus=1;
+    F.bit-=(1<<31);
+  }
+  //check infinity / NaN
+  if((F.bit>>23)==0b011111111){
+    if(frac==0){//infinity
+      ans=0b01110000;
+      if(minus){ans+=(1<<7);}
+      return ans;
+    }
+    else{//NaN
+      ans=0b01111111;
+      if(minus){ans+=(1<<7);}
+      return ans;
+    }
+  }
 
 
+  //Rounding
+  int Gbitindex=19;
+  int Gbit= (frac>>Gbitindex) & 1;
+  int Rbit= (frac>>(Gbitindex-1)) & 1;
+  int Sbit= (frac & (0x0003ffff));
+  int exp =( F.bit>>(Gbitindex+4));
+    
+  if((Rbit!=0 && Sbit!=0) || (Gbit!=0 && Rbit!=0 && Sbit==0)){
+    F.bit += (1<<Gbitindex);}
+  int newExp=(F.bit>>(Gbitindex+4));
+  if(newExp!=exp)Gbitindex++;
+  
 
-	return 9;
+  //trim out invalid exp range
+  exp=newExp;
+  if(exp>130){//infinity
+    ans=0b01110000;
+    if(minus){ans+=(1<<7);}
+    return ans;
+  }
+  if(exp<=119){//zero
+    ans=0b00000000;
+    if(minus){ans+=(1<<7);}
+    return ans;
+  }
+
+  //norm to denorm 
+  if(exp<=124){
+    frac += (1<<23);
+    //round again
+    Gbitindex += (125-exp);//-2-(exp-127)
+    Gbit= (frac>>Gbitindex)&1;
+    Rbit= (frac>>(Gbitindex-1))&1;
+    Sbit= frac & ((1<<(Gbitindex-1))-1);
+
+    if((Rbit != 0 && Sbit !=0) || (Gbit!=0 && Rbit!=0 && Sbit==0)){
+      frac+=(1<<Gbitindex);
+    }
+
+    for(int i=3;i>-1;i--){
+      ans+=((frac>>(Gbitindex+i))&1)<<i;
+    }
+    if(minus){ans+=(1<<7);}
+    return ans;
+  }
+
+  //norm to norm
+
+  ans+=((exp-124)<<4);
+  ans += (((F.bit>>(Gbitindex+3))&1)<<3)+(((F.bit>>(Gbitindex+2))&1)<<2)+(((F.bit>>(Gbitindex+1))&1)<<1)+((F.bit>>Gbitindex)&1);
+ 
+
+
+  if(minus){ans+=(1<<7);}
+  return ans;
 }
 
 
 float tinyfp2float(tinyfp t)
 {
+  //declare values
+  union bitfloat ans;
+  ans.bit = 0x00000000;
+  int minus=0;
 
+  //check sign
+  if((t>>7)){
+    minus=1;
+    t-=(1<<7);
+  }
 
+  //check infinity / NaN
+  if((t>>4)==0b0111){
+    int fflag = t & 0b00001111;
+    if(fflag==0){//infinity
+      ans.bit=0x7f800000;
+      if(minus){ans.bit+=(1<<31);}
+      return ans.flo;
+    }
+    else{//NaN
+      ans.bit=0x7fffffff;
+      if(minus){ans.bit+=(1<<31);}
+      return ans.flo;
+    }
+  }
 
-	return 9.9;
+  //copy all binary value in t to the ans
+  int exp = (t>>4)+124;
+  int frac = t & 0b00001111;
+  int Gbit = 19;
+  if(exp==124 && frac==0){if(minus){ans.bit+=(1<<31);}return ans.flo;}//zero
+  if(exp==124 && frac!=0){//denormalized value
+    if(frac>=8){
+      frac-=8;
+      Gbit++;
+    }
+    else if(frac>=4){
+      frac-=4;
+      Gbit+=2;
+      exp--;
+    }
+    else if(frac>=2){
+      frac-=2;
+      Gbit+=3;
+      exp-=2;
+    }
+    else if(frac==1){
+      frac-=1;
+      Gbit+=4;
+      exp-=3;
+    }
+  }
+  ans.bit += (exp<<23);
+  ans.bit += frac<<Gbit;
+
+  if(minus){ans.bit+=(1<<31);}
+  return ans.flo;
 }
